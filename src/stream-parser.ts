839d2eb13ae4e6693e8a310800cacf2ef12b29ee
MooYeol Prescott Lee <mooyoul@gmail.com>
2019-10-03 05:00:20 +0900

    style: lint

--- src/stream-parser.ts
+++ src/stream-parser.ts
@@ -1,9 +1,11 @@
 import * as Stream from "stream";
 const StreamParser = require("stream-parser") as any; // tslint:disable-line
 
-export interface IStreamParserWritable { // tslint:disable-line
+// tslint:disable
+export interface IStreamParserWritable {
   new(...args: any[]): IStreamParserWritableBase;
 }
+// tslint:enable
 
 export interface IStreamParserWritableBase { // tslint:disable-line
   _bytes(n: number, cb: (buf: Buffer) => void): void;
